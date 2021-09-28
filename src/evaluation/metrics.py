92b656e38305c7b747992d335e38d14dc09d7f4a
shahine <shahine96@gmail.com>
2021-09-28 17:19:26 +0100

    added metrics computation over standardized predictions + normalized RMSE and MAE

--- src/evaluation/metrics.py
+++ src/evaluation/metrics.py
@@ -16,12 +16,28 @@ def compute_scores(prediction_3d, groundtruth_3d, targets_2d, aggregate_fn):
         type: Description of returned object.
 
     """
+    # Compute metrics over raw predictions
     scores_2d = compute_2d_aggregate_metrics(prediction_3d, targets_2d, aggregate_fn)
     scores_3d_isotropic = compute_3d_isotropic_metrics(prediction_3d, groundtruth_3d)
     scores_3d_vertical = compute_3d_vertical_metrics(prediction_3d, groundtruth_3d)
-    output = {'2d': scores_2d,
-              '3d': {'isotropic': scores_3d_isotropic,
-                     'vertical': scores_3d_vertical}
+
+    # Compute metrics over standardized predictions
+    sigma_2d = targets_2d.std()
+    sigma_3d = groundtruth_3d.std()
+    std_scores_2d = compute_2d_aggregate_metrics(prediction_3d.div(sigma_2d), targets_2d.div(sigma_2d), aggregate_fn)
+    std_scores_3d_isotropic = compute_3d_isotropic_metrics(prediction_3d.div(sigma_3d), groundtruth_3d.div(sigma_3d))
+    std_scores_3d_vertical = compute_3d_vertical_metrics(prediction_3d.div(sigma_3d), groundtruth_3d.div(sigma_3d))
+
+    # Encapsulate scores into output dictionnary
+    output = {'raw': {'2d': scores_2d,
+                      '3d': {'isotropic': scores_3d_isotropic,
+                             'vertical': scores_3d_vertical}
+                      },
+
+              'std': {'2d': std_scores_2d,
+                      '3d': {'isotropic': std_scores_3d_isotropic,
+                             'vertical': std_scores_3d_vertical}
+                      }
               }
     return output
 
@@ -39,16 +55,30 @@ def compute_2d_aggregate_metrics(prediction_3d, targets_2d, aggregate_fn):
         type: dict[float]
 
     """
-    # Get prediction into shape that the aggregation function expects
+    # Get prediction into shape that the aggregation function expects and aggregate
     n_col = prediction_3d.size(0) * prediction_3d.size(1) * prediction_3d.size(2)
     prediction_3d = prediction_3d.reshape(n_col, -1)
     aggregate_prediction_2d = aggregate_fn(prediction_3d.unsqueeze(-1)).squeeze().flatten()
 
+    # Compute raw distances metrics
     difference = aggregate_prediction_2d.sub(targets_2d.flatten())
     rmse = torch.square(difference).mean().sqrt()
     mae = torch.abs(difference).mean()
+
+    # Compute normalized distances metrics
+    q25, q75 = torch.quantile(targets_2d, torch.tensor(q=[0.25, 0.75]))
+    nrmse = rmse.div(q75 - q25)
+    nmae = mae.div(q75 - q25)
+
+    # Compute spearman correlation
     corr = spearman_correlation(aggregate_prediction_2d.flatten(), targets_2d.flatten())
-    output = {'rmse': rmse.item(), 'mae': mae.item(), 'corr': corr}
+
+    # Encapsulate results in output dictionnary
+    output = {'rmse': rmse.item(),
+              'mae': mae.item(),
+              'nrmse': nrmse.item(),
+              'nmae': nmae.item(),
+              'corr': corr}
     return output
 
 
@@ -64,11 +94,25 @@ def compute_3d_isotropic_metrics(prediction_3d, groundtruth_3d):
         type: dict[float]
 
     """
+    # Compute raw distances metrics
     difference = prediction_3d.sub(groundtruth_3d)
     rmse = torch.square(difference).mean().sqrt()
     mae = torch.abs(difference).mean()
+
+    # Compute normalized distances metrics
+    q25, q75 = torch.quantile(groundtruth_3d, torch.tensor(q=[0.25, 0.75]))
+    nrmse = rmse.div(q75 - q25)
+    nmae = mae.div(q75 - q25)
+
+    # Compute spearman correlation
     corr = spearman_correlation(prediction_3d.flatten(), groundtruth_3d.flatten())
-    output = {'rmse': rmse.item(), 'mae': mae.item(), 'corr': corr}
+
+    # Encapsulate results in output dictionnary
+    output = {'rmse': rmse.item(),
+              'mae': mae.item(),
+              'nrmse': nrmse.item(),
+              'nmae': nmae.item(),
+              'corr': corr}
     return output
 
 
@@ -95,8 +139,17 @@ def compute_3d_vertical_metrics(prediction_3d, groundtruth_3d):
     mae = torch.abs(difference).mean(dim=-1).mean()
     corr = np.mean([spearman_correlation(pred, gt) for (pred, gt) in zip(prediction_3d, groundtruth_3d)])
 
-    # Return metrics dictionnary
-    output = {'rmse': rmse.item(), 'mae': mae.item(), 'corr': float(corr)}
+    # Compute normalized distances metrics
+    q25, q75 = torch.quantile(groundtruth_3d, torch.tensor(q=[0.25, 0.75]))
+    nrmse = rmse.div(q75 - q25)
+    nmae = mae.div(q75 - q25)
+
+    # Encapsulate results in output dictionnary
+    output = {'rmse': rmse.item(),
+              'mae': mae.item(),
+              'nrmse': nrmse.item(),
+              'nmae': nmae.item(),
+              'corr': float(corr)}
     return output
 
 
